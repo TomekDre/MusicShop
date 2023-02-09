52a065c64a71ab9ac81f23a20bedc99c6e795982
TomekDre <drewniak.tomasz@gmail.com>
2023-02-09 20:56:25 +0100

    Added mvc to app

---
+++ MusicShop/MusicShop/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "MusicShop";
+        }
+
+
+    }
+}
